5be8ef427f3913aff890f66612797cbb5b2d0068
Otilia Torgby <otiliatorgby@gmail.com>
2025-08-08 12:40:43 +0000

    Add Interfaces application

--- InterfaceApp/Program.cs
+++ InterfaceApp/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+// Interface for anything that can move
+interface IMovable
+{
+    void Move();
+}
+
+// Car class implementing IMovable
+class Car : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Car is moving");
+    }
+}
+
+//Bicycle class implementing IMovable
+class Bicycle : IMovable
+{
+    public void Move()
+    {
+        Console.WriteLine("Bicycle is moving");
+    }
+}
+
+class Program
+{
+    static void Main()
+    {
+        // Creating objects
+        Car myCar = new Car();
+        Bicycle myBicycle = new Bicycle();
+
+        // Calling the Move method on each
+        myCar.Move();      // Output: Car is moving
+        myBicycle.Move();  // Output: Bicycle is moving
+
+        // Keep window open for VS Code users
+        Console.ReadLine();
+    }
+}
